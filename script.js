b6a167aae1fab0bc3d6a3f449d3e0af5605d8aed
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-02-16 18:43:55 -0300

    update code

--- script.js
+++ script.js
@@ -7,21 +7,40 @@ function addStyle(styleString) {
 
 addStyle(`
     .clickedin_v1 {
-    height: 100%;
-    background-color: red;
-    width: 100%;
-    display: block;
-    position: absolute;
-    z-index: 9999999;
-    opacity: 0.5;
-    top: 0;
-    left: 0;
+        height: 100%;
+        width: 100%;
+        display: block;
+        position: absolute;
+        z-index: 1000;
+        top: 0;
+        left: 0;
+        display: grid;
+        place-items: center;
+    }
+
+    .clickedin_v1 svg {
+        width: 80px;
+        height: 80px;
+        transition: all .5s ease;
+        opacity: 0;
+    }
+
+    .clickedin__liked svg {
+        opacity: 1;
+    }
+
+    .clickedin_v1 svg > * {
+        fill: white;
     }
 
     .clickedin_v1__iframe {
         margin: 40px;
         height: calc(100% - 80px);
         width: calc(100% - 80px);
+    }
+
+    header {
+        z-index: 999 !important;
     }
 `);
 
@@ -44,6 +63,7 @@ function apply() {
             if (!content) return;
 
             const span = document.createElement('span');
+            span.innerHTML = likeIcon();
             span.classList.add('clickedin_v1');
             content.style.position = 'relative';
             content.appendChild(span);
@@ -69,6 +89,11 @@ function apply() {
                 const button = post.querySelector('.reactions-react-button button');
                 open = false;
                 button.click();
+                span.classList.add('clickedin__liked');
+
+                setTimeout(() => {
+                    span.classList.remove('clickedin__liked');
+                }, 1000);
             }
 
         } else {
@@ -94,3 +119,10 @@ try {
     ob.observe(document.querySelector('.scaffold-finite-scroll__content'),
         { childList: true, subtree: true })
 } catch { }
+
+function likeIcon() {
+    return `<?xml version="1.0" encoding="utf-8"?>
+    <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M15.9 4.5C15.9 3 14.418 2 13.26 2c-.806 0-.869.612-.993 1.82-.055.53-.121 1.174-.267 1.93-.386 2.002-1.72 4.56-2.996 5.325V17C9 19.25 9.75 20 13 20h3.773c2.176 0 2.703-1.433 2.899-1.964l.013-.036c.114-.306.358-.547.638-.82.31-.306.664-.653.927-1.18.311-.623.27-1.177.233-1.67-.023-.299-.044-.575.017-.83.064-.27.146-.475.225-.671.143-.356.275-.686.275-1.329 0-1.5-.748-2.498-2.315-2.498H15.5S15.9 6 15.9 4.5zM5.5 10A1.5 1.5 0 0 0 4 11.5v7a1.5 1.5 0 0 0 3 0v-7A1.5 1.5 0 0 0 5.5 10z" fill="#000000"/>
+    </svg>`
+}
